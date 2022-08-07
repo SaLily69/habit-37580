3acabfb31fa4cd2f196200419cdd1d9a1a1793af
SaLily69 <Hyuri.proS8@gmail.com>
2022-08-07 22:05:49 +0900

    フロント実装準備
    トップページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'goals/index'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "goals#index"
+  resouces :goals, only:[:index]
 end
