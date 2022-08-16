4bd1b1ebf42dd5dfeab6c7e56571bc264fde8e71
SaLily69 <Hyuri.proS8@gmail.com>
2022-08-16 14:20:52 +0900

    goalテーブル作成
    ※トップページビュー仮置き

--- db/migrate/20220815080331_create_goals.rb
+++ db/migrate/20220815080331_create_goals.rb
@@ -1,7 +1,10 @@
 class CreateGoals < ActiveRecord::Migration[6.0]
   def change
     create_table :goals do |t|
-
+      t.string     :theme              ,null: false
+      t.string     :purpose            ,null: false
+      t.time       :target_total_time
+      t.references :user               ,null: false ,foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_06_131351) do
+ActiveRecord::Schema.define(version: 2022_08_15_080331) do
+
+  create_table "goals", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "theme", null: false
+    t.string "purpose", null: false
+    t.time "target_total_time"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_goals_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -25,4 +35,5 @@ ActiveRecord::Schema.define(version: 2022_08_06_131351) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "goals", "users"
 end
