11dbbd1823fa3fbc967ed63dcb99a9ae5a611997
SaLily69 <Hyuri.proS8@gmail.com>
2022-08-18 20:03:58 +0900

    学習目標管理投稿：マイページ
    ※データベース修正

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,4 +1,14 @@
 class GoalsController < ApplicationController
   def index
+    #@goal = Goal.find(current_user.id)
   end
+
+  def new
+    @goal = Goal.new
+  end
+
+  def create
+    Goal.create(goal_params)
+  end
+
 end
