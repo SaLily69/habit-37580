adcf6b22cae528645d559f19161f71cc78d04c1f
SaLily69 <Hyuri.proS8@gmail.com>
2022-08-27 18:13:22 +0900

    学習目標管理一覧機能・編集機能一部実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'goals/index'
   devise_for :users
   root to: "goals#index"
-  resources :goals, only:[:index, :new, :create]
+  resources :goals, only:[:index, :new, :create, :edit, :update]
 end
