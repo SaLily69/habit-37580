8d44974f77d3293484956e7415bcff4cdfd5c94a
SaLily69 <Hyuri.proS8@gmail.com>
2022-09-03 18:55:11 +0900

    学習目標管理一覧機能追加
    トップページから学習記録ページへ遷移できる

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,2 +1,15 @@
 class LogsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    Log.create(log_params)
+  end
+
+  private
+  def log_params
+    params.require(:log).permit(:study_day,:study_hour,:study_minute,:task,:feed_back,:time_remind).merge(goal_id: params[:goal_id])
+  end
+
 end
