aa0bcf2bd5092257af5e97c2d029c3e6c92c9733
SaLily69 <Hyuri.proS8@gmail.com>
2022-12-21 14:16:34 +0900

    グラフ描写jsファイルへ

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,5 +1,15 @@
-function post (){
-  console.log("イベント発火");
- };
- 
- window.addEventListener('load', post);
+window.addEventListener('load', function(){
+
+  const ctx = document.getElementById("achievment_graph")
+  const achievment_graph = new Chart(ctx, {
+    type: 'pie',
+    data: {
+      labels:['達成率','目標達成まで残り'],
+      datasets:[{
+        label: '達成率',
+        backgroundColor: ['#00ffff','#f0ffff'],
+        data: gon.graph_data,
+      }]
+    },
+  })
+})
